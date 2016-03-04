fa07f593dcafa6abb512fe8135b1e53fb4e796de
Eric Mandel <eric@cfa.harvard.edu>
2016-03-04 12:02:33 -0500

    add image blend plugin to core plugins

---
+++ plugins/core/blend.js
@@ -0,0 +1,257 @@
+/*
+ * image blend module (February 25, 2016)
+ */
+
+/*jslint bitwise: true, plusplus: true, sloppy: true, vars: true, white: true, browser: true, devel: true, continue: true, unparam: true, regexp: true */
+/*global $, jQuery, JS9, sprintf */
+
+// make a module so as not to pollute the global namespace
+var BLEND;
+if( BLEND && (typeof BLEND !== "object" || BLEND.CLASS) ){
+    throw new Error("Namespace 'BLEND' already exists");
+}
+
+// create our namespace, and specify some meta-information and params
+BLEND = {};
+BLEND.CLASS = "JS9";      // class of plugins (1st part of div class)
+BLEND.NAME = "Blend";     // name of this plugin (2nd part of div class)
+BLEND.WIDTH =  512;	  // width of light window
+BLEND.HEIGHT = 270;	  // height of light window
+BLEND.BASE = BLEND.CLASS + BLEND.NAME;  // CSS base class name
+
+BLEND.blendModeHTML='When <b>Image Blending</b> is turned on, the images you select below will be combined using your chosen blend mode and optional opacity. See <a href="https://www.w3.org/TR/compositing-1/" target="blank">W3C Compositing and Blending</a> for info about compositing and blending.<p> <input type="checkbox" id="active" name="Image Blending" value="active" onclick="javascript:BLEND.xblendmode(\'%s\', this)" checked><b>Image Blending</b>';
+
+BLEND.imageHTML="<span style='float: left'>$active &nbsp;&nbsp; $blend &nbsp;&nbsp; $opacity</span>&nbsp;&nbsp; <span id='blendFile'>$imfile</span>";
+
+BLEND.activeHTML='<input class="blendActiveCheck" type="checkbox" id="active" name="active" value="active" onclick="javascript:BLEND.xactive(\'%s\', this)">blend using:';
+
+BLEND.blendHTML='<select id="blendModeSelect" onfocus="this.selectedIndex=0;" onchange="BLEND.xblend(\'%s\',this)"><option selected disabled>blend mode</option><option value="normal">normal</option><option value="screen">screen</option><option value="multiply">multiply</option><option value="overlay">overlay</option><option value="darken">darken</option><option value="lighten">lighten</option><option value="color-dodge">color-dodge</option><option value="color-burn">color-burn</option><option value="hard-light">hard-light</option><option value="soft-light">soft-light</option><option value="difference">difference</option><option value="exclusion">exclusion</option><option value="hue">hue</option><option value="saturation">saturation</option><option value="color">color</option> <option value="luminosity">luminosity</option></select>';
+
+BLEND.opacityHTML='<select id="blendOpacitySelect" onfocus="this.selectedIndex=0;" onchange="BLEND.xopacity(\'%s\',this)"><option selected disabled>opacity</option><option value="1.0">opaque</option><option value="0.95">0.95</option><option value="0.9">0.90</option><option value="0.85">0.85</option><option value="0.8">0.80</option><option value="0.75">0.75</option><option value="0.7">0.70</option><option value="0.65">0.65</option><option value="0.6">0.60</option><option value="0.55">0.55</option><option value="0.5">0.50</option><option value="0.45">0.45</option><option value="0.4">0.40</option><option value="0.35">0.35</option><option value="0.3">0.30</option><option value="0.25">0.25</option><option value="0.2">0.20</option><option value="0.1">0.10</option><option value="0.0">transparent</option></select>';
+
+// BLEND.imfileHTML='<input type="button" value="%s" onclick="javascript:BLEND.ximfile(\'%s\', this)">';
+BLEND.imfileHTML='<b>%s</b>';
+
+BLEND.nofileHTML='<p><span id="blendNoFile">[Images will appear here as they are loaded]</span>';
+
+// change active state
+BLEND.xactive = function(id, target){
+    var im = JS9.lookupImage(id);
+    var active = target.checked;
+    if( im ){
+	// change active mode
+	im.blendImage(active);
+    }
+};
+
+// change blend mode
+BLEND.xblend = function(id, target){
+    var im = JS9.lookupImage(id);
+    var blend = target.options[target.selectedIndex].value;
+    if( im ){
+	// change the blend mode
+	if( blend !== "" ){
+            im.blendImage(blend);
+	}
+    }
+};
+
+// change opacity
+BLEND.xopacity = function(id, target){
+    var im = JS9.lookupImage(id);
+    var opacity = target.options[target.selectedIndex].value;
+    if( im ){
+	// change opacity
+	if( opacity !== "" ){
+            im.blendImage(null, parseFloat(opacity));
+	}
+    }
+};
+
+// change current file
+BLEND.ximfile = function(id, target){
+    var im = JS9.lookupImage(id);
+    if( im ){
+	// change "current" file to display
+	im.displayImage();
+    }
+};
+
+// change global blend mode for this display
+BLEND.xblendmode = function(id, target){
+    var display = JS9.lookupDisplay(id);
+    var blendMode = target.checked;
+    // change global blend mode
+    if( display ){
+        JS9.BlendDisplay(blendMode, {display: display});
+	$(".blendActive").prop("disabled", !blendMode);
+    }
+};
+
+// get a BlendImage id based on the file image id
+BLEND.imid = function(im){
+    return im.id.replace(/\./g, "_").replace(/\[.*\]/g,"")  + "BlendImage";
+};
+
+// change the active image
+BLEND.activeImage = function(im){
+    var id;
+    if( im ){
+	id = BLEND.imid(im);
+	$("." + BLEND.BASE + "Image")
+	    .removeClass(BLEND.BASE + "ImageActive")
+	    .addClass(BLEND.BASE + "ImageInactive");
+	$("#" + id)
+	    .removeClass(BLEND.BASE + "ImageInactive")
+	    .addClass(BLEND.BASE + "ImageActive");
+    }
+};
+
+// add an image to the list of available images
+BLEND.addImage = function(im){
+    var s, id, divjq;
+    var opts = [];
+    if( !im ){
+	return;
+    }
+    id = BLEND.imid(im);
+    // value to pass to the macro expander
+    opts.push({name: "imid", value: im.id});
+    opts.push({name: "active", value: sprintf(BLEND.activeHTML, im.id)});
+    opts.push({name: "opacity", value: sprintf(BLEND.opacityHTML, im.id)});
+    opts.push({name: "imfile", value: sprintf(BLEND.imfileHTML, im.id, im.id)});
+    opts.push({name: "blend", value: sprintf(BLEND.blendHTML, im.id)});
+    // remove initial message
+    if( !this.blendDivs ){
+	this.blendImageContainer.html("");
+    }
+    // create the html for this image
+    s = JS9.Image.prototype.expandMacro.call(im, BLEND.imageHTML, opts);
+    // add image html to the image container
+    divjq = $("<div>")
+	.addClass(BLEND.BASE + "Image")
+	.addClass(BLEND.BASE + "ImageActive")
+	.attr("id", id)
+	.prop("imid", im.id)
+	.html(s)
+	.appendTo(this.blendImageContainer);
+    divjq.on("mousedown touchstart", function(evt){
+	    im.displayImage();
+	    BLEND.activeImage.call(this, im);
+    });
+    // save this div in the 
+    this.blendDivs++;
+    //make it the current one
+    BLEND.activeImage(im);
+};
+
+// remove an image to the list of available images
+BLEND.removeImage = function(im){
+    var id;
+    if( im ){
+	id = BLEND.imid(im);
+	$("#" + id).remove();
+	this.blendDivs--;
+	if( this.blendDivs === 0 ){
+	    this.blendImageContainer.html(BLEND.nofileHTML);
+	}
+	return true;
+    }
+    return false;
+};
+
+// constructor: it's here that you add HTML elements to the container div
+// But you don't have to do so: you can put the HTML elements right into
+// the Web page, in which case this routine can be empty.
+// Obviously, you must use the contructor to add your HTML elements if you
+// want to support your plugin from the View menu.
+//
+// The examaple below splits the difference: it allows the container div to
+// be empty or filled with the inner canvas, and thus support the view menu.
+BLEND.init = function(){
+    var i, im;
+    // on entry, these elements have already been defined:
+    // this.div:      the DOM element representing the div for this plugin
+    // this.divjq:    the jquery object representing the div for this plugin
+    // this.id:       the id ofthe div (or the plugin name as a default)
+    // this.display:  the display object associated with this plugin
+    // this.dispMode: display mode (for internal use)
+    //
+    // create container to hold image container and header
+    var that = this;
+    // no images/divs loaded yet
+    this.blendDivs = 0;
+    // main container
+    this.blendContainer = $("<div>")
+	.addClass(BLEND.BASE + "Container")
+	.attr("id", this.id + "BlendContainer")
+        .height(this.divjq.height()-4)
+        .width(this.divjq.width()-4)
+        .css("overflow", "auto")
+	.appendTo(this.divjq);
+    // header
+    this.blendHeader = $("<div>")
+	.addClass(BLEND.BASE + "Header")
+	.attr("id", this.display.id + "Header")
+	.html(sprintf(BLEND.blendModeHTML, this.display.id))
+	.appendTo(this.blendContainer);
+    // container to hold images
+    this.blendImageContainer = $("<div>")
+	.addClass(BLEND.BASE + "ImageContainer")
+	.attr("id", this.id + "BlendImageContainer")
+        .html(BLEND.nofileHTML)
+	.appendTo(this.blendContainer);
+    // start with blend mode turned on
+    this.display.blendMode = true;
+    // add currently loaded images
+    for(i=0; i<JS9.images.length; i++){
+	im = JS9.images[i];
+	if( im.display === this.display ){
+	    BLEND.addImage.call(this, im);
+	}
+    }
+    // the images within the image container will be sortable
+    this.blendImageContainer.sortable({
+	start: function(event, ui) {
+	    this.oidx = ui.item.index();
+	},
+	stop: function(event, ui) {
+	    var nidx = ui.item.index();
+	    // JS9 image list reflects the sort
+	    JS9.images.splice(nidx, 0, JS9.images.splice(this.oidx, 1)[0]);
+	    // redisplay in case something changed
+	    if( that.display.image ){
+		that.display.image.displayImage();
+	    }
+	}
+    });
+};
+
+// callback when an image is loaded
+BLEND.imageload = function(im){
+    // im gives access to image object
+    if( im ){
+	BLEND.addImage.call(this, im);
+    }
+};
+
+// callback when image is displayed
+BLEND.imagedisplay = function(im){
+    BLEND.activeImage.call(this, im);
+};
+
+// callback when image is displayed
+BLEND.imageclose = function(im){
+    BLEND.removeImage.call(this, im);
+};
+
+// add this plugin into JS9
+JS9.RegisterPlugin(BLEND.CLASS, BLEND.NAME, BLEND.init,
+		   {menuItem: "Image Blending",
+		    onimageload: BLEND.imageload,
+		    onimagedisplay: BLEND.imagedisplay,
+		    onimageclose: BLEND.imageclose,
+		    help: "help/blend.html",
+		    winTitle: "Image Blending",
+		    winDims: [BLEND.WIDTH, BLEND.HEIGHT]});
+
