33b0e484da52b1fc23c6f2d9450c30da823c6004
Eric Mandel <eric@cfa.harvard.edu>
2016-12-15 11:28:25 -0500

    improvements to regions and other 2d graphics:   -- update fabric.js to v1.6.6   -- don't use forEachObject in shape selection (breaks when removing shapes)   -- allow selection of regions based on color names   -- allow selection of regions based on JS9 shape (instead of fabric type)

--- js9.js
+++ js9.js
@@ -7904,9 +7904,8 @@ JS9.Fabric.addShapes = function(layerName, shape, opts){
 // select a one of more shapes by id or tag and execute a callback
 // call using image context
 JS9.Fabric.selectShapes = function(layerName, id, cb){
-    var i, group, ginfo, shape, sobj;
+    var i, group, ginfo, sobj, canvas, objects, olen, obj;
     var that = this;
-    var canvas;
     // sanity check
     if( !this.layers || !layerName || !this.layers[layerName] ){
 	return null;
@@ -7932,7 +7931,10 @@ JS9.Fabric.selectShapes = function(layerName, id, cb){
 	}
 	break;
     case "number":
-	canvas.forEachObject(function(obj){
+	objects = canvas.getObjects();
+        olen = objects.length;
+	while( olen-- ){
+	    obj = objects[olen];
 	    if( obj.params && (id === obj.params.id) ){
 		if( group && group.contains(obj) ){
 		    ginfo.group = group;
@@ -7941,12 +7943,10 @@ JS9.Fabric.selectShapes = function(layerName, id, cb){
 		}
 		cb.call(that, obj, ginfo);
 	    }
-	});
+	}
 	break;
     case "string":
-	// string id can be a region tag, color, or tag
-	// convert region name to graphics subsystem name
-	shape = id.toLowerCase().replace("box", "rect");
+	// string id can be a region tag, color, shape, or tag
 	// look for id in various ways
         if( id === "selected" ){
 	    if( canvas.getActiveObject() ){
@@ -7959,42 +7959,49 @@ JS9.Fabric.selectShapes = function(layerName, id, cb){
 		}
 	    } else if( group ){
 		ginfo.group = group;
-		group.forEachObject(function(obj){
-		    // member of a selected group
+		objects = group.getObjects();
+		olen = objects.length;
+		while( olen-- ){
+		    obj = objects[olen];
 		    if( obj.params ){
 			cb.call(that, obj, ginfo);
 		    }
-		});
+		}
 	    }
 	} else {
-	    canvas.forEachObject(function(obj){
-	      if( obj.params ){
-		if( group && group.contains(obj) ){
-		    ginfo.group = group;
-		} else {
-		    ginfo.group = null;
-		}
-		if( id === "all" ){
-		    // all
-		    cb.call(that, obj, ginfo);
-		} else if( id === obj.stroke ){
-		    // color
-		    cb.call(that, obj, ginfo);
-		} else if( shape === obj.type ){
-		    // shape
-		    cb.call(that, obj, ginfo);
-		} else {
-		    // tags
-		    if( obj.params.tags){
-			for(i=0; i<obj.params.tags.length; i++){
-			    if( id === obj.params.tags[i] ){
-				cb.call(that, obj, ginfo);
+	    // can't use forEachObject, which loops in ascending order,
+	    // because a "remove" cb changes the array destructively!
+	    objects = canvas.getObjects();
+            olen = objects.length;
+	    while( olen-- ){
+		obj = objects[olen];
+		if( obj.params ){
+		    if( group && group.contains(obj) ){
+			ginfo.group = group;
+		    } else {
+			ginfo.group = null;
+		    }
+		    if( id === "all" ){
+			// all
+			cb.call(that, obj, ginfo);
+		    } else if( JS9.colorToHex(id) === obj.stroke.toLowerCase()){
+			// color
+			cb.call(that, obj, ginfo);
+		    } else if( id === obj.params.shape ){
+			// shape
+			cb.call(that, obj, ginfo);
+		    } else {
+			// tags
+			if( obj.params.tags){
+			    for(i=0; i<obj.params.tags.length; i++){
+				if( id === obj.params.tags[i] ){
+				    cb.call(that, obj, ginfo);
+				}
 			    }
 			}
 		    }
 		}
-	      }
-	    });
+	    }
 	}
 	break;
     }
@@ -11669,6 +11676,43 @@ JS9.Starbase = function(s, opts){
     for(i = 0; i < this.headline.length; i++){
 	this[this.headline[i]] = i;
     }
+};
+
+// http://stackoverflow.com/questions/1573053/javascript-function-to-convert-color-names-to-hex-codes
+JS9.colorToHex = function(colour){
+    var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
+    "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
+    "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
+    "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
+    "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
+    "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
+    "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
+    "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
+    "honeydew":"#f0fff0","hotpink":"#ff69b4",
+    "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
+    "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
+    "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
+    "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
+    "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
+    "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
+    "navajowhite":"#ffdead","navy":"#000080",
+    "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
+    "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
+    "rebeccapurple":"#663399","red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
+    "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
+    "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
+    "violet":"#ee82ee",
+    "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
+    "yellow":"#ffff00","yellowgreen":"#9acd32"};
+    var c;
+    if( !colour ){
+	return;
+    }
+    c = colour.toLowerCase();
+    if( typeof colours[c] !== 'undefined' ){
+        return colours[c];
+    }
+    return colour;
 };
 
 // ---------------------------------------------------------------------
