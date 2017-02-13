74544b41a2cae5c8a9415bcf0c43af1e2a6aeaa7
Eric Mandel <eric@cfa.harvard.edu>
2017-02-13 16:16:48 -0500

    fix use with remote host (-h)

--- threeways/threeways.sh
+++ threeways/threeways.sh
@@ -21,7 +21,7 @@ js9 $XARGS BlendImage 'screen' 1 true > /dev/null
 sleep $timeout
 
 echo 'load ./blend/galex.fits'
-../js9load './blend/galex.fits' '{"scale":"log","colormap":"green","contrast":6.25,"bias":0.25}'
+../js9load $XARGS './blend/galex.fits' '{"scale":"log","colormap":"green","contrast":6.25,"bias":0.25}'
 sleep $timeout
 
 echo 'reproject using chandra wcs'
@@ -31,7 +31,7 @@ js9 $XARGS BlendImage 'screen' 1 true > /dev/null
 sleep $timeout
 
 echo 'load ./blend/spitzer.fits'
-../js9load './blend/spitzer.fits' '{"scale":"log","colormap":"blue","contrast":6.3,"bias":0.54}'
+../js9load $XARGS './blend/spitzer.fits' '{"scale":"log","colormap":"blue","contrast":6.3,"bias":0.54}'
 sleep $timeout
 
 echo 'reproject using chandra wcs'
