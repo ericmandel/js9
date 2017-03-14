f68550727eb0145d87a9ffd4246e0e929ff1c3b5
Eric Mandel <eric@cfa.harvard.edu>
2017-03-14 10:24:45 -0400

    utilize js9Prefs.js:   -- use js9Prefs.js instead of js9Prefs.json in js9debug.html   -- create default and sao versions of js9Prefs.js   -- install js9Prefs.js (along with js9Prefs.json) js9Prefs.json uses synchronous XHR, a technique which is deprecated in FF, so we are preparing to deprecate js9Prefs.json as well.

---
+++ js9Prefs-sao.js
@@ -0,0 +1,14 @@
+var JS9Prefs = {
+  "globalOpts": {"helperType":	     "nodejs",
+  		 "helperPort":       2718, 
+		 "helperCGI":        "./cgi-bin/js9/js9Helper.cgi",
+		 "fits2png":         false,
+		 "debug":	     0,
+		 "workDir":	     "./tmp",
+		 "loadProxy":	     true,
+		 "dataPath":	     "$HOME/Desktop:$HOME/data",
+		 "analysisPlugins":  "./analysis-plugins",
+		 "analysisWrappers": "./analysis-wrappers"},
+  "imageOpts":  {"colormap":	     "grey",
+  		 "scale":     	     "log"}
+}
