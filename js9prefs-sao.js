f4163b8a5529b93c68989f58941560fcbf3d4a11
Eric Mandel <eric@cfa.harvard.edu>
2017-04-16 13:59:21 -0400

    default for loadProxy should be false (but true for SAO install)

--- js9prefs-sao.js
+++ js9prefs-sao.js
@@ -1,9 +1,11 @@
 var JS9Prefs = {
   "globalOpts": {"helperType":	     "nodejs",
   		 "helperPort":       2718, 
-		 "helperCGI":        "./cgi-bin/js9Helper.cgi",
+		 "helperCGI":        "./cgi-bin/js9/js9Helper.cgi",
 		 "fits2png":         false,
 		 "debug":	     0,
+		 "loadProxy":	     true,
+		 "workDir":	     "./tmp",
 		 "dataPath":	     "$HOME/Desktop:$HOME/data",
 		 "analysisPlugins":  "./analysis-plugins",
 		 "analysisWrappers": "./analysis-wrappers"},
