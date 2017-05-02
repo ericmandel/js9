916bf12a54a9b53552cf02028d174eb86d331134
Eric Mandel <eric@cfa.harvard.edu>
2017-05-02 07:38:51 -0400

    turn loadProxy on by default (for Desktop use)

--- js9Helper.js
+++ js9Helper.js
@@ -63,8 +63,6 @@ var globalOpts = {
     maxBinaryBuffer:  150*1024000, // exec buffer: good for 4096^2 64-bit image
     maxTextBuffer:    5*1024000,   // exec buffer: good for text
     textEncoding:     "ascii",     // encoding for returned stdout from exec
-    workDir:          "",          // top-level working directory for exec
-    workDirQuota:     100,         // quota on working directory (Mb)
     rmWorkDir:        true,        // remove workdir on disconnect?
     remoteMsgs:       1 // 0 => none, 1 => samehost, 2 => all
 };
