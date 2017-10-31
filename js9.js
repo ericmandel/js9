ef533459d5c7942e69132bc3a0b9ad4409ae9364
Sam Verstocken <sam.verstocken@ugent.be>
2017-10-31 18:46:24 +0100

    Rolled back addRegions change for good

--- js9.js
+++ js9.js
@@ -17208,19 +17208,7 @@ JS9.mkPublic("NewShapeLayer", function(layer, opts){
 JS9.mkPublic("AddRegions", function(region, opts){
         
     var obj = JS9.parsePublicArgs(arguments);
-    
-    // check for display
-    if( obj.display ){
-    display = obj.display; } 
-    else {
-    if( JS9.displays.length > 0 ){
-   	    display = JS9.displays[0].id;
-    	} else {
-    	    display = JS9.DEFID;
-        }
-    }
-    
-    var im = JS9.getImage(display);
+    var im = JS9.getImage(obj.display);
     if( im ){
 	region = obj.argv[0];
 	opts = obj.argv[1];
