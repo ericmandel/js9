c820872a657ef19edbc0babec00e8ff0d78a6485
Eric Mandel <eric@cfa.harvard.edu>
2017-11-13 14:19:01 -0500

    more fiddling with threeways demo

--- threeways/threeways.sh
+++ threeways/threeways.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #set -x
 
+# blend image directory relative to web page
+BLEND="./blend"
+
 timeout=3
 XARGS=""
 while [ x"$1" != x ]; do
     case $1 in
+    -b) shift
+	BLEND="$1"
+        shift;;
     -h) shift
 	XARGS="$XARGS -h $1"
         shift;;
@@ -28,10 +34,10 @@ dowait (){
         ASK='Continue? '
     fi
     if [ x"$doyesno" = xtrue ]; then
-        read -p "$ASK" -n 1 -r
-	echo
+        read -p "$ASK"
         if [[ ! $REPLY =~ ^[Yy]$ ]]
         then
+echo "${REPLY}" | od -a
             exit
         fi
     else
@@ -42,31 +48,25 @@ dowait (){
 
 js9 $XARGS BlendDisplay false > /dev/null
 
-echo 'load ../blend/chandra.fits'
-../js9load $XARGS '../blend/chandra.fits' '{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}'
-js9 $XARGS BlendImage 'screen' 1 true > /dev/null
+echo "load chandra.fits"
+../js9load $XARGS "${BLEND}/chandra.fits" '{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}'
+js9 $XARGS BlendImage "screen" 1 true > /dev/null
 
-dowait 'load ../blend/galex.fits'
-../js9load $XARGS '../blend/galex.fits' '{"scale":"log","colormap":"green","contrast":6.25,"bias":0.25}'
+dowait "load galex.fits"
+../js9load $XARGS "${BLEND}/galex.fits" '{"scale":"log","colormap":"green","contrast":6.5,"bias":0.375}'
 
-dowait 'reproject using chandra wcs'
-js9 $XARGS ReprojectData 'chandra.fits' > /dev/null
-js9 $XARGS SetColormap 'green' 5.6 0.74 > /dev/null
-js9 $XARGS BlendImage 'screen' 1 true > /dev/null
+dowait "reproject galex using the chandra wcs"
+js9 $XARGS ReprojectData "chandra.fits" > /dev/null
+js9 $XARGS SetColormap "green" 5.6 0.74 > /dev/null
+js9 $XARGS BlendImage "screen" 1 true > /dev/null
 
-dowait 'load ../blend/spitzer.fits'
-../js9load $XARGS '../blend/spitzer.fits' '{"scale":"log","colormap":"blue","contrast":6.3,"bias":0.54}'
+dowait "load spitzer.fits"
+../js9load $XARGS "${BLEND}/spitzer.fits" '{"scale":"log","colormap":"blue","contrast":4.89,"bias":0.41}'
 
-dowait 'reproject using chandra wcs'
-js9 $XARGS ReprojectData 'chandra.fits' > /dev/null
-js9 $XARGS SetColormap 'blue' 6.3 0.54 > /dev/null
-js9 $XARGS BlendImage 'screen' 1 true > /dev/null
+dowait "reproject spitzer using the chandra wcs"
+js9 $XARGS ReprojectData "chandra.fits" > /dev/null
+js9 $XARGS SetColormap "blue" 6.3 0.54 > /dev/null
+js9 $XARGS BlendImage "screen" 1 true > /dev/null
 
-dowait 'blend all three images'
+dowait "blend all three images"
 js9 $XARGS BlendDisplay true > /dev/null
-
-dowait 'separate into three displays'
-js9 $XARGS SeparateDisplay true > /dev/null
-
-dowait 'gather again into one display'
-js9 $XARGS GatherDisplay true > /dev/null
