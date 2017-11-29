c0c690d226655f5247ae127ea777b448cf2ee51c
Eric Mandel <eric@cfa.harvard.edu>
2017-11-29 15:29:21 -0500

    improve threeways shell demo

--- threeways/threeways.sh
+++ threeways/threeways.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-#set -x
+# set -x
 
 # blend image directory relative to web page
 BLDIR="./blend"
 
+DOADDON=false
 TIMEOUT=1
 XARGS=""
 ZOOM=1
 while [ x"$1" != x ]; do
     case $1 in
     -a) shift
-	doapp=true;;
+        DOADDON=true;;
     -b) shift
 	BLDIR="$1"
         shift;;
     -h) shift
 	XARGS="$XARGS -h $1"
+        shift;;
+    -i) shift
+	XARGS="$XARGS --id $1"
         shift;;
     -j) BLDIR="./js9/blend"
 	TIMEOUT=0
@@ -47,7 +51,6 @@ dowait (){
         read -p "$ASK"
         if [[ ! $REPLY =~ ^[Yy]$ ]]
         then
-echo "${REPLY}" | od -a
             exit
         fi
     else
@@ -56,45 +59,56 @@ echo "${REPLY}" | od -a
     fi
 }
 
-if [ x${doapp} = xtrue ]; then
-  js9 -a &
-  sleep 1
-fi
+xjs9 (){
+  echo "    js9 $*" >&2
+  js9 $XARGS $*
+}
 
+xjs9load (){
+  echo "    js9load $*" >&2
+  js9load $XARGS $*
+}
+
+echo "start processing"
 # js9 $XARGS ResizeDisplay 1024 > /dev/null
-js9 $XARGS BlendDisplay false > /dev/null
+xjs9 $XARGS BlendDisplay false > /dev/null
+xjs9 $XARGS CloseImage chandra.fits > /dev/null
+xjs9 $XARGS CloseImage galex.fits > /dev/null
+xjs9 $XARGS CloseImage spitzer.fits > /dev/null
+xjs9 $XARGS CloseImage hubble.fits > /dev/null
 
-echo "load chandra.fits"
-../js9load $XARGS "${BLDIR}/chandra.fits" '{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}'
-js9 $XARGS SetZoom $ZOOM > /dev/null
-js9 $XARGS BlendImage "screen" 1 true > /dev/null
+dowait "load chandra.fits"
+xjs9load $XARGS "${BLDIR}/chandra.fits" '{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}'
+xjs9 $XARGS SetZoom $ZOOM > /dev/null
+xjs9 $XARGS BlendImage "screen" 1 true > /dev/null
 
 dowait "load galex.fits"
-../js9load $XARGS "${BLDIR}/galex.fits" '{"scale":"log","colormap":"green","contrast":6.5,"bias":0.375}'
+xjs9load $XARGS "${BLDIR}/galex.fits" '{"scale":"log","colormap":"green","contrast":6.5,"bias":0.375}'
 
 dowait "reproject galex using the chandra wcs"
-js9 $XARGS ReprojectData "chandra.fits" > /dev/null
-js9 $XARGS SetColormap "green" 5.6 0.74 > /dev/null
-js9 $XARGS SetZoom $ZOOM > /dev/null
-js9 $XARGS BlendImage "screen" 1 true > /dev/null
+xjs9 $XARGS ReprojectData "chandra.fits" > /dev/null
+xjs9 $XARGS SetColormap "green" 5.6 0.74 > /dev/null
+xjs9 $XARGS SetZoom $ZOOM > /dev/null
+xjs9 $XARGS BlendImage "screen" 1 true > /dev/null
 
 dowait "load spitzer.fits"
-../js9load $XARGS "${BLDIR}/spitzer.fits" '{"scale":"log","colormap":"blue","contrast":4.89,"bias":0.41}'
+xjs9load $XARGS "${BLDIR}/spitzer.fits" '{"scale":"log","colormap":"blue","contrast":4.89,"bias":0.41}'
 
 dowait "reproject spitzer using the chandra wcs"
-js9 $XARGS ReprojectData "chandra.fits" > /dev/null
-js9 $XARGS SetColormap "blue" 6.3 0.54 > /dev/null
-js9 $XARGS SetZoom $ZOOM > /dev/null
-js9 $XARGS BlendImage "screen" 1 true > /dev/null
+xjs9 $XARGS ReprojectData "chandra.fits" > /dev/null
+xjs9 $XARGS SetColormap "blue" 6.3 0.54 > /dev/null
+xjs9 $XARGS SetZoom $ZOOM > /dev/null
+xjs9 $XARGS BlendImage "screen" 1 true > /dev/null
 
-dowait "load hst.fits"
-../js9load $XARGS "${BLDIR}/hst.fits" '{"scale":"log","colormap":"magma","contrast":6.32,"bias":0.384}'
-
-dowait "reproject hst using the chandra wcs"
-js9 $XARGS ReprojectData chandra.fits > /dev/null
-js9 $XARGS SetColormap cool 5.74 0.38 > /dev/null
-js9 $XARGS SetZoom $ZOOM > /dev/null
-js9 $XARGS BlendImage "screen" 1 true > /dev/null
+if [ x$DOADDON = xtrue ]; then
+    dowait "load hst.fits"
+    xjs9load $XARGS "${BLDIR}/hst.fits" '{"scale":"log","scaleclipping":"user","scalemin":0,"scalemax":5,"colormap":"heat","contrast":4.0,"bias":0.67}'
+    dowait "reproject hst using the chandra wcs"
+    xjs9 $XARGS ReprojectData chandra.fits > /dev/null
+    xjs9 $XARGS SetColormap 3.0 0.535 > /dev/null
+    xjs9 $XARGS SetZoom $ZOOM > /dev/null
+    xjs9 $XARGS BlendImage "screen" 1 true > /dev/null
+fi
 
 dowait "blend the images"
-js9 $XARGS BlendDisplay true > /dev/null
+xjs9 $XARGS BlendDisplay true > /dev/null
