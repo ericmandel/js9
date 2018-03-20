55396d00964193de7a4eed358d317dbbc97aaa17
Eric Mandel <eric@cfa.harvard.edu>
2018-03-20 19:56:24 -0400

    tweak demos

--- threeways/threeways.py
+++ threeways/threeways.py
@@ -4,6 +4,8 @@ import time
 timeout = 3
 loadTimeout = 1
 maxIter = 10
+id = "myJS9"
+
 def waitLoad():
     iter = 0
     done = False
@@ -17,51 +19,68 @@ def waitLoad():
                 raise ValueError("timeout waiting")
             time.sleep(loadTimeout)
 
-j = pyjs9.JS9()
+print 'pyjs9.JS9(id=' + id + ')'
+j = pyjs9.JS9(id=id)
 j.BlendDisplay(False)
 
-print 'load ./blend/chandra.fits'
-j.Load('./blend/chandra.fits', '{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}')
+print 'load chandra.fits ...'
+print '    j.Load("../blend/chandra.fits", \'{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}\')'
+j.Load('../blend/chandra.fits', '{"scale":"log","colormap":"red","contrast":5.78,"bias":0.15}')
 waitLoad()
+print '    j.SetZoom(2)'
 j.SetZoom(2)
+print '    j.BlendImage("screen", 1, True)'
 j.BlendImage('screen', 1, True)
 time.sleep(timeout)
 
-print 'load ./blend/galex.fits'
-j.Load('./blend/galex.fits', '{"scale":"log","colormap":"green","contrast":6.25,"bias":0.25}')
+print 'load galex.fits ...'
+print '    j.Load("../blend/galex.fits", \"{"scale":"log","colormap":"green","contrast":6.25,"bias":0.25}\')'
+j.Load('../blend/galex.fits', '{"scale":"log","colormap":"green","contrast":6.25,"bias":0.25}')
 waitLoad()
 time.sleep(timeout)
 
-print 'reproject using chandra wcs'
+print '    j.ReprojectData("chandra.fits")'
 j.ReprojectData('chandra.fits')
+print '    j.SetColormap("green", 5.6, 0.74)'
 j.SetColormap('green', 5.6, 0.74)
+print '    j.SetZoom(2)'
 j.SetZoom(2)
+print '    j.BlendImage("screen", 1, True)'
 j.BlendImage('screen', 1, True)
 time.sleep(timeout)
 
-print 'load ./blend/spitzer.fits'
-j.Load('./blend/spitzer.fits', '{"scale":"log","colormap":"blue","contrast":6.3,"bias":0.54}')
+print 'load spitzer.fits ...'
+print '    j.Load("../blend/spitzer.fits", \'{"scale":"log","colormap":"blue","contrast":6.3,"bias":0.54}\')'
+j.Load('../blend/spitzer.fits', '{"scale":"log","colormap":"blue","contrast":6.3,"bias":0.54}')
 waitLoad()
 time.sleep(timeout)
 
-print 'reproject using chandra wcs'
+print '    j.ReprojectData("chandra.fits")'
 j.ReprojectData('chandra.fits')
+print '    j.SetColormap("blue", 6.3, 0.54)'
 j.SetColormap('blue', 6.3, 0.54)
+print '    j.SetZoom(2)'
 j.SetZoom(2)
+print '    j.BlendImage("screen", 1, True)'
 j.BlendImage('screen', 1, True)
 time.sleep(timeout)
 
-print 'load ./blend/hst.fits'
-j.Load('./blend/hst.fits', '{"scale":"log","scaleclipping":"user","scalemin":0,"scalemax":5,"colormap":"heat","contrast":4.0,"bias":0.67}')
+print 'load hst.fits ...'
+print '    j.Load("../blend/hst.fits", \'{"scale":"log","scaleclipping":"user","scalemin":0,"scalemax":5,"colormap":"heat","contrast":4.0,"bias":0.67}\')'
+j.Load('../blend/hst.fits', '{"scale":"log","scaleclipping":"user","scalemin":0,"scalemax":5,"colormap":"heat","contrast":4.0,"bias":0.67}')
 waitLoad()
 time.sleep(timeout)
 
-print 'reproject using chandra wcs'
+print '    j.ReprojectData("chandra.fits")'
 j.ReprojectData('chandra.fits')
+print '    j.SetColormap("heat", 3.0, 0.535)'
 j.SetColormap('heat', 3.0, 0.535)
+print '    j.SetZoom(2)'
 j.SetZoom(2)
+print '    j.BlendImage("screen", 1, True)'
 j.BlendImage('screen', 1, True)
 time.sleep(timeout)
 
 print 'blend the images'
+print '    j.BlendDisplay(True)'
 j.BlendDisplay(True)
