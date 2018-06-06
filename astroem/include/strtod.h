06910afb0287acaf13f068beacfe3b2fe8f52729
Eric Mandel <eric@cfa.harvard.edu>
2018-06-06 14:19:17 -0400

    astroem: move strtod.c to util librarary

---
+++ astroem/include/strtod.h
@@ -0,0 +1,18 @@
+/*
+ *	Copyright (c) 2015 Smithsonian Astrophysical Observatory
+ */
+
+/*
+ *
+ * strtod.h -- declarations for SAOstrtod()
+ *
+ */
+
+#ifndef	__strtod_h
+#define	__strtod_h
+
+extern int SAOdtype;
+
+double SAOstrtod(char *s, char **t);
+
+#endif
---
+++ astroem/include/xerror.h
@@ -0,0 +1,28 @@
+/*
+ *	Copyright (c) 2015 Smithsonian Astrophysical Observatory
+ */
+
+/*
+ *
+ * xerror.h -- declarations for xerror handling
+ *
+ */
+
+#ifndef	__xerror_h
+#define	__xerror_h
+
+#include "xutil.h"
+#ifdef __STDC__
+#include <stdarg.h>
+#else
+#include <varargs.h>
+#endif
+
+char *xerrorstring(void);
+int setxerror(int flag);
+void xerror(FILE *fd, char *format, ...);
+char *xwarningstring(void);
+int setxwarning(int flag);
+void xwarning(FILE *fd, char *format, ...);
+
+#endif
