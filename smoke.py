f177d8f85d4229e39052608bff23bff835f2cf7c
Eric Mandel <eric@cfa.harvard.edu>
2019-10-05 20:29:32 -0400

    add non-wcs image to flip test

--- smoke.py
+++ smoke.py
@@ -516,6 +516,9 @@ def flipRotateTest(j):
     displayMessage(j, 'j.LoadRegions("casa/casa.reg")')
     j.LoadRegions("casa/casa.reg")
     flipAll(j)
+    loadImage(j, 'fits/squares.fits', {"colormap":"grey"});
+    j.AddRegions('physical; polygon(438.00, 24.00, 498.00, 24.00, 468.00, 84.00) {"text":"white","textOpts":{"px":466,"py":97}}; box(52.00, 452.00, 60.00, 60.00, 0.0000) {"text":"black","textOpts":{"px":52,"py":470}}; circle(459.00, 462.00, 30.00) {"text":"darkgrey","textOpts":{"px":459,"py":421}}; ellipse(57.00, 43.00, 30.00, 20.00, 0.0000) {"text":"lightgrey","textOpts":{"px":57,"py":77}}')
+    flipAll(j)
     sleep()
     closeDisplay(j)
 
@@ -528,6 +531,7 @@ def blendTest(j):
     j.BlendDisplay(False)
     loadImage(j, 'blend/chandra.fits',
               '{"scale":"linear","colormap":"sls","contrast":5.78,"bias":0.15}')
+    sleep()
     displayMessage(j, 'j.SetScale(log)')
     j.SetScale("log")
     displayMessage(j, 'j.GetScale()')
