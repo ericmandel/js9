fcca4ed3616db16ff64c869bb3ffc5ef979b60fc
Eric Mandel <eric@cfa.harvard.edu>
2020-02-05 08:18:24 -0500

    binning: improve labels, grey out filter for image data

--- js9plugins.js
+++ js9plugins.js
@@ -865,7 +865,6 @@ module.exports = xhr;
 
 },{}]},{},[1]);
 
-/*jslint white: true, vars: true, plusplus: true, nomen: true, unparam: true */
 /*globals $, JS9, Fitsy */
 
 "use strict";
@@ -873,13 +872,13 @@ module.exports = xhr;
 (function() {
 
     function reBinImage(div, display) {
-	var hdu, opts, npos;
-	var im   = JS9.GetImage({display: display});
-	var form = $(div).find(".binning-form")[0];
-	var rebin = function(im, hdu, display){
-	    var ss;
-	    var rexp = /(\[.*[a-zA-Z0-9_].*\])\[.*\]/;
-	    var topts = {display: display};
+	let hdu, opts, npos;
+	let im   = JS9.GetImage({display: display});
+	let form = $(div).find(".binning-form")[0];
+	let rebin = function(im, hdu, display){
+	    let ss;
+	    let rexp = /(\[.*[a-zA-Z0-9_].*\])\[.*\]/;
+	    let topts = {display: display};
 	    if( form.separate.checked ){
 		// replace old extensions with new
 		// would be better to combine them, but ...
@@ -949,9 +948,9 @@ module.exports = xhr;
     }
 
     function centerBinImage(xdim, ydim, div, display) {
-	var im   = JS9.GetImage({display: display});
-	var form = $(div).find(".binning-form")[0];
-	var fdims = im.fileDimensions();
+	let im   = JS9.GetImage({display: display});
+	let form = $(div).find(".binning-form")[0];
+	let fdims = im.fileDimensions();
 	form.xcen.value = 0;
 	form.ycen.value = 0;
 	if( xdim > 0 ){
@@ -968,8 +967,8 @@ module.exports = xhr;
     }
 
     function getBinParams(div, display) {
-	var im, ipos, lpos, form, hdu, bin;
-	var binval1, binval2;
+	let im, ipos, lpos, form, hdu, bin;
+	let binval1, binval2;
 	if ( display === undefined ) {
 	    div     = this.div;
 	    display = this.display;
@@ -1019,14 +1018,22 @@ module.exports = xhr;
 		    form.bin.value = String(bin);
 		    form.xdim.value = String(Math.floor(hdu.naxis1 * bin));
 		    form.ydim.value = String(Math.floor(hdu.naxis2 * bin));
-		    form.filter.value = im.raw.filter || "";
-
+		    if( JS9.globalOpts.enableImageFilter ){
+			form.filter.value = im.raw.filter || "";
+		    } else {
+			form.filter.value = "";
+		    }
 		    form.bin.disabled = false;
 		    form.xcen.disabled = false;
 		    form.ycen.disabled = false;
 		    form.xdim.disabled = false;
 		    form.ydim.disabled = false;
-		    form.filter.disabled = false;
+		    if( JS9.globalOpts.enableImageFilter ){
+			form.filter.disabled = false;
+		    } else {
+			form.filter.disabled = true;
+			form.filter.style.backgroundColor="#E0E0E0";
+		    }
 		}
 	    } else {
 		form.rebin.disabled = true;
@@ -1035,63 +1042,68 @@ module.exports = xhr;
     }
 
     function binningInit() {
-	var that = this;
-	var div = this.div;
-	var display = this.display;
-	var win = this.winHandle;
-	var disclose = "";
-	var im  = JS9.GetImage({display: this.display});
+	let binblock;
+	let that = this;
+	let div = this.div;
+	let display = this.display;
+	let win = this.winHandle;
+	let disclose = "";
+	let im  = JS9.GetImage({display: this.display});
 
 	if( !im || (im && !im.raw.hdu) ){
 	    div.innerHTML = '<p style="padding: 5px"><center>FITS image sections, with binning and filtering</center>';
 	    return;
 	}
 
+	if( im.imtab === "image" ){
+	    binblock = "block";
+	} else {
+	    binblock = "bin";
+	}
+
 	if( !win ){
 	    disclose = 'disabled="disabled"';
 	}
 
-	/*eslint-disable no-multi-str */
-	$(div).html('<form class="binning-form js9Form" style="margin: 0px; padding: 8px; width: 100%; height: 100%">	\
-	    <table style="margin:0px; cellspacing:0; border-collapse:separate; border-spacing:4px 10px;">       \
-	           <tr>	<td><input type=button class=full-image value="Load full image" style="text-align:right;"></td>	\
-			<td>&nbsp;</td>    								\
-			<td>&nbsp;</td>								    	\
-			<td>&nbsp;</td> 								\
-		   </tr>										        \
-	           <tr>	<td><b>center:</b></td>								\
-			<td><input type=text name=xcen size=10 style="text-align:right;"></td>		\
-			<td><input type=text name=ycen size=10 style="text-align:right;"></td>    	\
-			<td>&nbsp(center position of section)</td>						\
-		   </tr>										\
-	           <tr>	<td><b>size:</b></td>								\
-			<td><input type=text name=xdim size=10 style="text-align:right;"></td>		\
-			<td><input type=text name=ydim size=10 style="text-align:right;"></td>		\
-			<td>&nbsp(width, height of section)</td>						\
-		   </tr>										\
-                   <tr>	<td><b>bin:</b></td>							\
-			<td><input type=text name=bin value=1 size=10 style="text-align:right;"></td>	\
-			<td></td>									\
-			<td>&nbsp(apply bin factor to section)</td>						\
-		   </tr>										\
-	           <tr>	<td><b>filter:</b></td>								\
-			<td colspan="2"><textarea name=filter rows="1" cols="22" style="text-align:left;" autocapitalize="off" autocorrect="off"></textarea></td>	\
-			<td>&nbsp(event/row filter for tables)</td>						\
-		   </tr>										\
-	           <tr>	<td><b>separate:</b></td>			\
-                        <td><input type=checkbox name=separate class="sep-image" style="text-align:left;"></td>	\
-			<td></td>									\
-			<td>&nbsp(display as separate image?)</td>						\
-		   </tr>										\
-		   <tr>											\
-			<td><input type=button name=rebin value="Run" class="rebin-image"></td>         \
-			<td>&nbsp;</td>									\
-			<td>&nbsp;</td>									\
-                        <td>&nbsp;<input type=button name=close value="Close" class="close-image" ' + disclose + '></td> \
-		   </tr>										\
-	    </table>											\
-	    </form>');
-	/*eslint-enable no-multi-str */
+	$(div).html(`<form class="binning-form js9Form" style="margin: 0px; padding: 8px; width: 100%; height: 100%">
+	    <table style="margin:0px; cellspacing:0; border-collapse:separate; border-spacing:4px 10px;">
+	           <tr>	<td><input type=button class=full-image value="Load full image" style="text-align:right;"></td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+		   </tr>
+	           <tr>	<td><b>center:</b></td>
+			<td><input type=text name=xcen size=10 style="text-align:right;"></td>
+			<td><input type=text name=ycen size=10 style="text-align:right;"></td>
+			<td>&nbsp(center position of section)</td>
+		   </tr>
+	           <tr>	<td><b>size:</b></td>
+			<td><input type=text name=xdim size=10 style="text-align:right;"></td>
+			<td><input type=text name=ydim size=10 style="text-align:right;"></td>
+			<td>&nbsp(width, height of section)</td>
+		   </tr>
+                   <tr>	<td><b>${binblock}:</b></td>
+			<td><input type=text name=bin value=1 size=10 style="text-align:right;"></td>
+			<td></td>
+			<td>&nbsp(apply ${binblock} factor to ${im.imtab})</td>
+		   </tr>
+	           <tr>	<td><b>filter:</b></td>
+			<td colspan="2"><textarea name=filter rows="1" cols="22" style="text-align:left;" autocapitalize="off" autocorrect="off"></textarea></td>
+			<td>&nbsp(event/row filter for table)</td>
+		   </tr>
+	           <tr>	<td><b>separate:</b></td>
+                        <td><input type=checkbox name=separate class="sep-image" style="text-align:left;"></td>
+			<td></td>
+			<td>&nbsp(display as separate image?)</td>
+		   </tr>
+		   <tr>
+			<td><input type=button name=rebin value="Run" class="rebin-image"></td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+                        <td>&nbsp;<input type=button name=close value="Close" class="close-image" ${disclose}'></td>
+		   </tr>
+	    </table>
+	    </form>`);
 
 	// click doesn't work on localhost on a Mac using Chrome/Safari, but mouseup does ...
 	$(div).find(".full-image").on("mouseup", function ()  { centerBinImage(0, 0, div, display); });
