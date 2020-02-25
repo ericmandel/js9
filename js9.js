b84a32ef64440aefbb42ba8c9898fc4f907cb59d
Eric Mandel <eric@cfa.harvard.edu>
2020-02-25 14:20:28 -0500

    statusbar: begin cleanup and optimize updates

--- js9.js
+++ js9.js
@@ -228,10 +228,8 @@ JS9.globalOpts = {
     }, // keyboard actions
     mousetouchZoom: false,	// use mouse wheel, pinch to zoom?
     metaClickPan: true,         // metaKey + click pans to mouse position?
-    // statusBar: ["zoom", "scale", "colormap", "wcssys"],  // status display
-    // statusBar: "zoom: $zoom; scale: $scale/$scaleclipping($scalemin,$scalemax); color: $colormap($contrast,$bias); wcs: $wcssys($wcsunits)",  // status display
     // statusBar: "$mag; $scale($scaleclipping); $img(images/voyager/color_$colormap.png) $colormap; $wcssys",  // status display
-    statusBar: "$mag; $colorbar; $scale($scaleclipping,$scalemin,$scalemax); $wcssys",  // status display
+    statusBar: "$mag; $colorbar; $scale($scaleclipping:$scalemin,$scalemax); $wcssys",  // status display
     toolbarTooltips: false,     // display tooltips on toolbar?
     centerDivs: ["JS9Menubar"], // divs which take part in JS9.Display.center()
     resizeDivs: ["JS9Menubar", "JS9Colorbar", "JS9Toolbar", "JS9Statusbar"], // divs which take part in JS9.Display.resize()
--- js9plugins.js
+++ js9plugins.js
@@ -12093,7 +12093,7 @@ JS9.RegisterPlugin(JS9.Separate.CLASS, JS9.Separate.NAME, JS9.Separate.init,
  * status plugin (February 20, 2020)
  */
 
-/*global $, sprintf, JS9 */
+/*global $, JS9 */
 
 "use strict";
 
@@ -12103,8 +12103,8 @@ JS9.Statusbar.CLASS = "JS9";      // class of plugins (1st part of div class)
 JS9.Statusbar.NAME = "Statusbar"; // name of this plugin (2nd part of div class)
 JS9.Statusbar.WIDTH =  512;       // width of light window
 JS9.Statusbar.HEIGHT = 28;        // height of light window
-JS9.Statusbar.COLORWIDTH =  100;  // width of colorbar, if present
-JS9.Statusbar.COLORHEIGHT = 14;        // height of colorbar, if present
+JS9.Statusbar.COLORWIDTH =  200;  // width of colorbar, if present
+JS9.Statusbar.COLORHEIGHT = 14;   // height of colorbar, if present
 JS9.Statusbar.BASE = JS9.Statusbar.CLASS + JS9.Statusbar.NAME;
 
 // when an item is clicked, we want to highlight it
@@ -12121,7 +12121,7 @@ JS9.Statusbar.xeq = function(target){
     $(target).removeClass("JS9StatusbarItemHighlight");
     $(target).addClass("JS9StatusbarItemNoHighlight");
     // look at the html for this element
-    s = $(target).html();
+    s = $(target).attr("name");
     if( s ){
 	// is there a hint about what sort of menu status it contains?
 	arr = s.match(/file|edit|view|zoom|scale|color|regions|wcs|analysis/i);
@@ -12154,37 +12154,44 @@ JS9.Statusbar.xeq = function(target){
 
 // redraw status on display
 JS9.Statusbar.display = function(im){
-    let i, status;
-    let s = "";
+    let i, s, arr, items;
     if( im && JS9.globalOpts.statusBar ){
-	if( $.isArray(JS9.globalOpts.statusBar) ){
-	    // array of parameter names
-	    for(i=0; i<JS9.globalOpts.statusBar.length; i++){
-		status = JS9.globalOpts.statusBar[i];
-		s += sprintf("<span class='JS9StatusbarItemContainer JS9StatusbarItemNoHighlight' onmousedown='JS9.Statusbar.setup(this)' onmouseup='JS9.Statusbar.xeq(this)'>%s: <span class='JS9StatusbarItemValue'>$%s</span></span>", status, status);
+	// escape brackets and parens before macro expansion, then unescape
+	s = JS9.globalOpts.statusBar
+	    .replace(/\(/g, " __OP__ ")
+	    .replace(/\)/g, " __CP__ ")
+	    .replace(/\[/g, " __OB__ ")
+	    .replace(/\]/g, " __CB__ ");
+	s = im.expandMacro(s)
+	    .replace(/ __OP__ /g, "(")
+	    .replace(/ __CP__ /g, ")")
+	    .replace(/ __OB__ /g, "[")
+	    .replace(/ __CB__ /g, "]");
+	if( !im.tmp.statusbar ){
+	    s = `<div class='JS9StatusbarItem JS9StatusbarItemNoHighlight' onmousedown='JS9.Statusbar.setup(this)' onmouseup='JS9.Statusbar.xeq(this)'>${s}</div>`
+		.replace(/; */g, "</div>&nbsp;<div class='JS9StatusbarItem JS9StatusbarItemNoHighlight' onmousedown='JS9.Statusbar.setup(this)' onmouseup='JS9.Statusbar.xeq(this)'>")
+		.replace(/\$img\(([^()]+)\)/g, "<img src='$1' name='$1' class='JS9StatusbarImageItem JS9StatusbarItemNoHighlight'>")
+		.replace(/\$colorbar/g, `<div name='JS9Colorbar' class='JS9Colorbar JS9StatusbarPluginItem' data-width="${this.colorwidth}px" data-height="${this.colorheight}px" data-showTicks="false" ></div>`);
+	    this.statusContainer.html(s);
+	    // give names to each item, based on input format
+	    items = this.divjq.find(`.JS9StatusbarItem`);
+	    arr = JS9.globalOpts.statusBar.split(";");
+	    for(i=0; i<arr.length; i++){
+		items.eq(i).attr("name", arr[i]);
 	    }
+	    // might have to run AddDivs, if we added a plugin
+	    if( JS9.globalOpts.statusBar.match(/\$colorbar/) ){
+		JS9.AddDivs({display: im});
+	    }
+	    im.tmp.statusbar = this;
 	} else {
-	    // formatted string
-	    // escape brackets and parens before macro expansion, then unescape
-	    s = JS9.globalOpts.statusBar
-	        .replace(/\(/g, " __OP__ ")
-	        .replace(/\)/g, " __CP__ ")
-	        .replace(/\[/g, " __OB__ ")
-	        .replace(/\]/g, " __CB__ ");
-	    s = im.expandMacro(s)
-	        .replace(/ __OP__ /g, "(")
-	        .replace(/ __CP__ /g, ")")
-	        .replace(/ __OB__ /g, "[")
-	        .replace(/ __CB__ /g, "]");
-	    // wrap in element containers
-	    s = `<div class='JS9StatusbarItemContainer JS9StatusbarItemNoHighlight' onmousedown='JS9.Statusbar.setup(this)' onmouseup='JS9.Statusbar.xeq(this)'>${s}</div>`
-	        .replace(/; */g, "</div>&nbsp;<div class='JS9StatusbarItemContainer JS9StatusbarItemNoHighlight' onmousedown='JS9.Statusbar.setup(this)' onmouseup='JS9.Statusbar.xeq(this)'>")
-	        .replace(/\$img\(([^()]+)\)/g, "<img src='$1' name='$1' class='JS9StatusbarImage JS9StatusbarImageOption JS9StatusbarItemNoHighlight'>")
-	        .replace(/\$colorbar/g, `<div name='JS9Colorbar' class='JS9Colorbar JS9StatusbarItemContainer JS9StatusbarPluginItem' data-width="${this.colorwidth}px" data-height="${this.colorheight}px" data-showTicks="false" ></div>`);
-	}
-	this.statusContainer.html(s);
-	if( JS9.globalOpts.statusBar.match(/\$colorbar/) ){
-	    JS9.AddDivs({display: im});
+	    items = this.divjq.find(`.JS9StatusbarItem`);
+	    arr = s.split(";");
+	    for(i=0; i<arr.length; i++){
+		if( !arr[i].match(/\$colorbar/) ){
+		    items.eq(i).html(arr[i]);
+		}
+	    }
 	}
     } else {
 	this.statusContainer.html("");
