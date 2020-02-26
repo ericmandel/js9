d39f16918d86b76b1e97fa1200efb537a58cb403
Eric Mandel <eric@cfa.harvard.edu>
2020-02-26 17:14:37 -0500

    menubar: it's empty.svg, not empty.png

--- plugins/core/menubar.js
+++ plugins/core/menubar.js
@@ -1967,7 +1967,7 @@ JS9.Menubar.createMenus = function(){
 		s1 = JS9.globalOpts.topColormaps[i];
 		s2 = s1;
 		if( JS9.globalOpts.menuImages ){
-		    hstr = `<div class='JS9MenubarImage' name='${s2}'><img src='${JS9.InstallDir("images")}/voyager/color_${s2}.png' name='color_${s2}' class='JS9MenubarImage JS9MenubarImageOption' onerror='this.src="images/empty.jpg"' >` + `&nbsp;&nbsp;${s2}</div>`;
+		    hstr = `<div class='JS9MenubarImage' name='${s2}'><img src='${JS9.InstallDir("images")}/voyager/color_${s2}.png' name='color_${s2}' class='JS9MenubarImage JS9MenubarImageOption' onerror='this.src="images/empty.svg"' >` + `&nbsp;&nbsp;${s2}</div>`;
 		    items[s1] = {name: hstr, isHtmlName: true};
 		} else {
 		    items[s1] = xname(s2);
