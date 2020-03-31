3fcfe7c714f72149a1f805fce5f9f646dfac6a63
Eric Mandel <eric@cfa.harvard.edu>
2020-03-31 12:11:11 -0400

    titlebar: desktop displays full pathname if available (not just id)

--- js9.js
+++ js9.js
@@ -18704,16 +18704,24 @@ JS9.Titlebar.init = function(opts){
 // change titlebar when image is loaded
 JS9.Titlebar.imageload = function(im){
     if( im && JS9.globalOpts.updateTitlebar ){
-	document.title = `${JS9.Titlebar.title}: ${im.id}`;
-	JS9.Titlebar.imid = im.id;
+	if( window.isElectron ){
+	    JS9.Titlebar.imid = im.fitsFile || im.file || im.id;
+	} else {
+	    JS9.Titlebar.imid = im.id;
+	}
+	document.title = `${JS9.Titlebar.title}: ${JS9.Titlebar.imid}`;
     }
 };
 
 // change titlebar when image is displayed
 JS9.Titlebar.imagedisplay = function(im){
     if( im && im.id !== JS9.Titlebar.imid && JS9.globalOpts.updateTitlebar ){
-	document.title = `${JS9.Titlebar.title}: ${im.id}`;
-	JS9.Titlebar.imid = im.id;
+	if( window.isElectron ){
+	    JS9.Titlebar.imid = im.fitsFile || im.file || im.id;
+	} else {
+	    JS9.Titlebar.imid = im.id;
+	}
+	document.title = `${JS9.Titlebar.title}: ${JS9.Titlebar.imid}`;
     }
 };
 
