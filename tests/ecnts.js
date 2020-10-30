519688b00f01483e8e8c3500a5a5acceef238524
Eric Mandel <eric@cfa.harvard.edu>
2020-10-30 08:27:22 -0400

    add test of hostfs-based scripting

---
+++ tests/ecnts.js
@@ -0,0 +1,35 @@
+// ecnts.js: test of hostfs ... display 3 sections and get counts in each
+let fs;
+try{
+    fs = require("fs");
+}
+catch(e){
+    JS9.error("Node.js 'fs' module is unavailable. Did you enable hostfs?");
+}
+
+JS9.Load("data/kes75/kes75_evt2.fits.gz", {onload: function(im){
+    let i;
+    let s = "";
+    let got = 0;
+    // energy filters
+    const f = ["energy=500:1500", "energy=1500:2500", "energy=2500:8000"];
+    // get counts in regions as each image is displayed
+    const getcnts = function(i){
+  	return function(xim){
+  	    s += xim.countsInRegions();
+  	    got++;
+  	    if( got === 3 ){
+                // write the results to a log file
+  		fs.writeFile("countsInRegions.log", s, function(err) {
+		    if( err ) { JS9.error(err); }
+  		}); 
+  	    }
+    	};
+    };
+    // process each of the event filters to make a separate image
+    for(i=0; i<f.length; i++){
+    	// display filtered image in a separate displayed
+    	JS9.DisplaySection({filter:f[i], separate:true,
+    			    ondisplaysection: getcnts(i)}, {display: im});
+    }
+}});
