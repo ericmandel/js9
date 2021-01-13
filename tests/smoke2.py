6c5651f380aaf31c39883f40075304b257992504
Eric Mandel <eric@cfa.harvard.edu>
2021-01-13 13:43:53 -0500

    smoke2: add some messages to the display

--- tests/smoke2.py
+++ tests/smoke2.py
@@ -8,48 +8,79 @@ from smokesubs import *
 
 # add new tests here
 
+timeout = 2
+
+regions = 'physical;ellipse(4103.0,3873.0,30.0,20.0,0) {"strokeWidth":2} # source,include,foo2; ellipse(3990.0,3872.0,30.0,20.0,0) {"strokeWidth":2} # source,include,foo1; circle(3990.0,3988.0,30.0) {"strokeWidth":2} # source,include,foo2; circle(3882.0,3988.0,30.0) {"strokeWidth":2} # source,include,foo1'
+
+def xwait(j):
+    sleep(timeout)
+    if len(j.GetRegions()) == 0:
+        j.AddRegions(regions)
+        sleep(timeout)
+
 def regSelectTest(j, file):
     """
     region selection (including boolean parser)
     """
-    timeout = 1
+    rtns = [j.ChangeRegions, j.SelectRegions]
     if file:
         closeImage(j)
         loadImage(j, file, '{"scale":"log", "colormap": "cool"}')
-    displayMessage(j, "add regions: circles and boxes")
-    j.AddRegions('physical;box(4103.0,3873.0,60.0,60.0,0) {"strokeWidth":2} # source,include,foo2; box(3990.0,3872.0,60.0,60.0,0) {"strokeWidth":2} # source,include,foo1; circle(3990.0,3988.0,30.0) {"strokeWidth":2} # source,include,foo2; circle(3882.0,3988.0,30.0) {"strokeWidth":2} # source,include,foo1')
-    sleep(timeout)
-    j.ChangeRegions("circle", {"color": "orange"})
-    sleep(timeout)
-    j.ChangeRegions("box", {"color": "magenta"})
-    sleep(timeout)
-    j.ChangeRegions("circle && foo1", {"color": "red"})
-    sleep(timeout)
-    j.ChangeRegions("box && !foo2", {"color": "red"})
-    sleep(timeout)
-    j.ChangeRegions("circle || box", {"color": "yellow"})
-    sleep(timeout)
-    j.ChangeRegions("(circle && foo2) || (box && foo1)", {"color": "red"})
-    sleep(timeout)
-    j.ChangeRegions("(circle && !foo2) || (box && !foo1)", {"color": "#00FF00"})
-    sleep(timeout)
-    j.ChangeRegions("/foo*/", {"color": "yellow"})
-    sleep(timeout)
-    j.ChangeRegions("circle && foo1", {"color": "magenta"})
-    sleep(timeout)
-    j.ChangeRegions("circle && foo2", {"prevselect": "add", "color": "magenta"})
-    sleep(timeout)
-    j.ChangeRegions("box && foo1", {"prevselect": "add", "color": "magenta"})
-    sleep(timeout)
-    j.ChangeRegions("box && foo2", {"prevselect": "add", "color": "magenta"})
-    sleep(timeout)
-    j.ChangeRegions("!foo1 && !circle", {"color": "yellow"})
-    sleep(timeout)
-    j.ChangeRegions("$prevselect || !foo2 && !circle", {"color": "yellow"})
-    sleep(timeout)
-    j.ChangeRegions("$prevselect || !foo1 && !box", {"color": "yellow"})
-    sleep(timeout)
-    j.ChangeRegions("$prevselect || !foo2 && !box", {"color": "yellow"})
+    for rtn in rtns:
+        j.AddRegions(regions)
+        xwait(j)
+        rtn("circle", {"color": "orange"})
+        displayMessage(j, "circle: orange");
+        xwait(j)
+        rtn("ellipse", {"color": "magenta"})
+        displayMessage(j, "ellipse: magenta");
+        xwait(j)
+        rtn("circle && foo1", {"color": "red"})
+        displayMessage(j, "circle && foo1: red");
+        xwait(j)
+        rtn("ellipse && !foo2", {"color": "red"})
+        displayMessage(j, "ellipse && !foo2: red");
+        xwait(j)
+        rtn("circle || ellipse", {"color": "yellow"})
+        displayMessage(j, "circle || ellipse: yellow");
+        xwait(j)
+        rtn("(circle && foo2) || (ellipse && foo1)", {"color": "red"})
+        displayMessage(j, "(circle && foo2) || (ellipse && foo1): red");
+        xwait(j)
+        rtn("(circle && !foo2) || (ellipse && !foo1)", {"color": "white"})
+        displayMessage(j, "(circle && !foo2) || (ellipse && !foo1): white");
+        xwait(j)
+        rtn("/foo*/", {"color": "red"})
+        displayMessage(j, "/foo*/: red");
+        xwait(j)
+        rtn("circle && foo1", {"color": "magenta"})
+        displayMessage(j, "circle && foo1: magenta");
+        xwait(j)
+        rtn("circle && foo2", {"prev": "add", "color": "orange"})
+        displayMessage(j, "circle && foo2 + prev: orange");
+        xwait(j)
+        rtn("ellipse && foo1", {"prev": "or", "color": "yellow"})
+        displayMessage(j, "ellipse && foo1 + prev: yellow");
+        xwait(j)
+        rtn("ellipse && foo2", {"prev": "add", "color": "cyan"})
+        displayMessage(j, "ellipse && foo2 + prev: cyan");
+        xwait(j)
+        rtn("previous", {"color": "red"})
+        displayMessage(j, "previous: red");
+        xwait(j)
+        rtn("!foo1 && !circle", {"color": "orange"})
+        displayMessage(j, "!foo1 && !circle: orange");
+        xwait(j)
+        rtn("previous || (!foo2 && !circle)", {"color": "yellow"})
+        displayMessage(j, "previous || (!foo2 && !circle): yellow");
+        xwait(j)
+        rtn("previous || (!foo1 && !ellipse)", {"color": "orange"})
+        displayMessage(j, "previous || (!foo1 && !ellipse): orange");
+        xwait(j)
+        rtn("previous || (!foo2 && !ellipse)", {"color": "magenta"})
+        displayMessage(j, "previous || (!foo2 && !ellipse): magenta");
+        sleep(timeout)
+        j.RemoveRegions("all")
 
 # end of new tests
 
