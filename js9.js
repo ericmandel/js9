4b036791f4fc7d4f53537fa6eaf4bef8e8635d3e
Eric Mandel <eric@cfa.harvard.edu>
2021-08-16 08:30:25 -0400

    patch release v3.6.1

--- js9.js
+++ js9.js
@@ -27,7 +27,7 @@ var JS9 = (function(){
 // module header
 const JS9 = {};
 JS9.NAME = "JS9";		// The name of this namespace
-JS9.VERSION = "3.6";		// The version of this namespace
+JS9.VERSION = "3.6.1";		// The version of this namespace
 JS9.COPYRIGHT = "Copyright (c) 2012-2021 Smithsonian Institution";
 JS9.ABOUT = `JS9 ${JS9.VERSION}: astronomical image display everywhere\nEric Mandel, Alexey Vikhlinin\ncontact: emandel@cfa.harvard.edu\n${JS9.COPYRIGHT}`;
 
